92d20c4c72e60cdff09eb80add05f90dd1c5b1a1
kmaalig <106278777+kmaalig@users.noreply.github.com>
2022-07-26 12:09:29 -0500

    Merged fetch request from separate file

--- jalopy-main/js/views/Movies.js
+++ jalopy-main/js/views/Movies.js
@@ -2,5 +2,17 @@ export default function MoviesView(props) {
     return `<h1>Movies</h1>`;
 }
 
-export function MoviesEvents() {
+export async function MoviesEvents() {
+    let movie = '';
+    fetch('https://ballistic-snapdragon-silica.glitch.me/movies')
+        .then(function (response) {
+            console.log('fetch complete')
+            if(response.status !== 200) {
+                console.log('fetch failed');
+            } else {
+                return response.json();
+            }
+        }).then(function(data) {
+            console.log(data);
+    });
 }
