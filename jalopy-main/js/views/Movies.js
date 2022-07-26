50f627f3e9ce689fbe6c77c29e4f0ada61031a8a
Ashley Martinez <ashley.martinez32@gmail.com>
2022-07-26 12:09:04 -0500

    addedhtml

--- jalopy-main/js/views/Movies.js
+++ jalopy-main/js/views/Movies.js
@@ -1,6 +1,25 @@
 export default function MoviesView(props) {
-    return `<h1>Movies</h1>`;
-}
+    return `
 
+<div class="container">
+    <h1>Quotes</h1>
+    <div class="card">
+        <div class="card-body">
+            <p class="dog-fact">Humans generally see better than dogs, but dogs see much better when there is low-light.</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <p class="dog-fact">All dogs can be traced back 40 million years ago to a weasel-like animal called the Miacis which dwelled in trees and dens. The Miacis later evolved into the Tomarctus, a direct forbear of the genus Canis, which includes the wolf and jackal as well as the dog.</p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <p class="dog-fact">Small quantities of grapes and raisins can cause renal failure in dogs. Chocolate, macadamia nuts, cooked onions, or anything with caffeine can also be harmful.</p>
+        </div>
+    </div>
+</div>
+`;
+}
 export function MoviesEvents() {
 }
